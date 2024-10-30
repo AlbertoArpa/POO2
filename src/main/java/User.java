14b71b983f2c84d76c9f466a48e06ac4f7630721
Largo Monteagudo, Adrian <adrian.largo@alumnos.upm.es>
2024-10-30 17:40:43 +0100

    Añadir algunas clases

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,8 @@
 public class User {
+    private String nameUser;
+    private String password;
+    public User(String nameUser, String password){
+        this.nameUser = nameUser;
+        this.password = password;
+    }
 }
