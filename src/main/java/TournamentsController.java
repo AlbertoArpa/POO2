a89e1615f1d1a9c455ee7a8e60f7eb5889b377c3
Alberto Arpa <a.arpa@alumnos.upm.es>
2024-10-31 19:11:27 +0100

    createTournament

--- src/main/java/TournamentsController.java
+++ src/main/java/TournamentsController.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class TournamentsController {
     private ArrayList<Tournament> tournaments;
@@ -7,7 +8,7 @@ public class TournamentsController {
         this.tournaments = new ArrayList<>();
     }
 
-    public Tournament existTournament(String name) {
+    public Tournament getTournament(String name) {
         for (Tournament tournament : tournaments) {
             if (tournament.getName().equals(name)) {
                 return tournament;
@@ -15,4 +16,14 @@ public class TournamentsController {
         }
         return null;
     }
+
+    public boolean createTournament(String name, Date startDate, Date endDate, String league, String sport, String categoryRank) {
+        boolean result = false;
+        if (getTournament(name) != null) {
+            tournaments.add(new Tournament<>(name, startDate, endDate, league, sport, categoryRank));
+            result = true;
+        }
+        return result;
+    }
+
 }
