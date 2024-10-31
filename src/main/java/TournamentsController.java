d0dab86056d6038aee43d67be31740d85ccc8b17
Javier Grande Alonso <franciscojaviergrandealonso@gmail.com>
2024-10-31 11:15:05 +0100

    solo un metodo para ver si asi esta bien. hay q revisar lo de los torneos y los genericos

--- src/main/java/TournamentsController.java
+++ src/main/java/TournamentsController.java
@@ -2,4 +2,13 @@ import java.util.List;
 
 public class TournamentsController {
     private List<Tournament> tournaments;
+
+    Tournament existTournament(String name) {
+        for (Tournament tournament : tournaments) {
+            if (tournament.getName().equals(name)) {
+                return tournament;
+            }
+        }
+        return null;
+    }
 }
