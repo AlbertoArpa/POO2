f0ecb7d6d80dae0cb1916db608eb8f4fcaecee7d
Javier Grande Alonso <franciscojaviergrandealonso@gmail.com>
2024-11-02 21:15:33 +0100

    tonteria de app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -96,11 +96,13 @@ public class App {
                     } else System.out.println("\nMUY POCOS ARGUMENTOS");
                     break;
                 case "team-delete": // GENERICOS
-/*                    if (arguments.length >= 1) {
+                /*                    
+                    if (arguments.length >= 1) {
                         if (teamsController.deleteTeam(arguments[0])) {
                             System.out.println("\nEQUIPO ");
                         } else System.out.println();
-                    } else System.out.println("MUY POCOS ARGUMENTOS");*/
+                    } else System.out.println("MUY POCOS ARGUMENTOS");
+                */
                     break;
                 case "team-add":
                     if (arguments.length >= 2) {
