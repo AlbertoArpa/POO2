26c26bfdf1231f2a0f601e4144a1a9632328349d
Largo Monteagudo, Adrian <adrian.largo@alumnos.upm.es>
2024-11-13 12:38:46 +0100

    Nuevas Clases

--- src/main/java/ModelException.java
+++ src/main/java/ModelException.java
@@ -1,6 +1,9 @@
-public class ModelException extends Exception{
-    private static final long STATIC_ID = 7322478738749203984L;
-    public ModelException(String message){
+import java.io.Serial;
 
+public class ModelException extends Exception{
+    @Serial
+    private static final long serialVersionUID = 7322478738749203984L;
+    public ModelException(String message) {
+        super(message);
     }
 }
