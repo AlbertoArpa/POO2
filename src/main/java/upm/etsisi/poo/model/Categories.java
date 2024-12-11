376cb4cc382f659fb1b8b1b60240e0405791be85
Largo Monteagudo, Adrian <adrian.largo@alumnos.upm.es>
2024-12-11 20:58:31 +0100

    DOCUMENTOS

--- src/main/java/upm/etsisi/poo/model/Categories.java
+++ src/main/java/upm/etsisi/poo/model/Categories.java
@@ -1,28 +1,28 @@
 package upm.etsisi.poo.model;
 
-public class Categories {
-    private enum categories {
-        SCORED_POINTS, ASSIST_POINTS, WON_GAMES, WON_TOURNAMENTS, MONEY_GENERATED
-    }
+public enum Categories {
+    SCORED_POINTS, ASSIST_POINTS, WON_GAMES, WON_TOURNAMENTS, MONEY_GENERATED;
 
     public static String getCategory(String category) {
         String categoryS = null;
-        category = category.toUpperCase();
-        if (category.contains(" ")) {
-            String[] arg = category.split(" ");
-            category = arg[0] + "_" + arg[1];
-        }
-        for (categories categoryA : categories.values()) {
-            if (categoryA.name().equals(category)) {
-                categoryS = category;
+        if (category!=null){
+            category = category.toUpperCase();
+            if (category.contains(" ")) {
+                String[] arg = category.split(" ");
+                category = arg[0] + "_" + arg[1];
+            }
+            for (Categories categoryA : Categories.values()) {
+                if (categoryA.name().equals(category)) {
+                    categoryS = category;
+                }
             }
         }
         return categoryS;
     }
 
     public static String[] getCategories() {
-        String[] categoryS = new String[categories.values().length];
-        for (categories categoryA : categories.values()) {
+        String[] categoryS = new String[Categories.values().length];
+        for (Categories categoryA : Categories.values()) {
             categoryS[categoryA.ordinal()] = categoryA.name();
         }
         return categoryS;
